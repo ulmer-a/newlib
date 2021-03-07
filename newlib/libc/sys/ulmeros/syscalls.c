4497afd29cd513befeec9b1f506ca2f6f20a9201
Alexander Ulmer <ulmer@student.tugraz.at>
2021-03-07 10:25:11 +0100

    move syscall() into custom assembly file

--- newlib/libc/sys/ulmeros/syscalls.c
+++ newlib/libc/sys/ulmeros/syscalls.c
@@ -9,32 +9,12 @@
 
 #include "syscalls.h"
 
-typedef unsigned long sreg_t;
-
-static inline long __syscall(sreg_t number,
-    sreg_t arg1, sreg_t arg2, sreg_t arg3,
-    sreg_t arg4, sreg_t arg5, sreg_t arg6)
-{
-    sreg_t ret = 0;
-    __asm__ volatile (
-        "mov %0, %%rax;"
-        "mov %1, %%r8;"
-        "mov %2, %%r9;"
-        "mov %3, %%r10;"
-        "mov %4, %%r11;"
-        "mov %5, %%r12;"
-        "mov %6, %%r13;"
-        "int $0x80;"
-        "mov %%rax, %0;"
-        : "=r"(ret)
-        : "g"(number),
-        "g"(arg1), "g"(arg2), "g"(arg3),
-        "g"(arg4), "g"(arg5), "g"(arg6)
-        : "rax", "r8", "r9",
-        "r10", "r11", "r12", "r13"
-    );
-    return ret;
-}
+ssize_t _ulmeros_syscall0(size_t number);
+ssize_t _ulmeros_syscall1(size_t number, size_t arg1);
+ssize_t _ulmeros_syscall2(size_t number, size_t arg1, size_t arg2);
+ssize_t _ulmeros_syscall3(size_t number, size_t arg1, size_t arg2, size_t arg3);
+ssize_t _ulmeros_syscall4(size_t number, size_t arg1, size_t arg2, size_t arg3, size_t arg4);
+ssize_t _ulmeros_syscall5(size_t number, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5);
 
 #define SYSCALL_RETURN_ERRNO(x) \
   if ((x) >= 0) { return ret; } \
@@ -43,49 +23,49 @@ static inline long __syscall(sreg_t number,
  
 void _exit(int status)
 {
-  __syscall(SYS_EXIT, status, 0, 0, 0, 0, 0);
+  _ulmeros_syscall1(SYS_EXIT, status);
 
   /* never reached */
 }
 
 int read(int file, char *ptr, int len)
 {
-  ssize_t ret = __syscall(SYS_READ, file, (sreg_t)ptr, len, 0, 0, 0);
+  ssize_t ret = _ulmeros_syscall3(SYS_READ, file, (size_t)ptr, len);
   SYSCALL_RETURN_ERRNO(ret);
 }
 
 int write(int file, char *ptr, int len)
 {
-  ssize_t ret = __syscall(SYS_WRITE, file, (sreg_t)ptr, len, 0, 0, 0);
+  ssize_t ret = _ulmeros_syscall3(SYS_WRITE, file, (size_t)ptr, len);
   SYSCALL_RETURN_ERRNO(ret);
 }
 
 int close(int file)
 {
-  ssize_t ret = __syscall(SYS_CLOSE, file, 0, 0, 0, 0, 0);
+  ssize_t ret = _ulmeros_syscall1(SYS_CLOSE, file);
   SYSCALL_RETURN_ERRNO(ret);
 }
 
 int open(const char *name, int flags, ...)
 {
-  ssize_t ret = __syscall(SYS_OPEN, (sreg_t)name, flags, 0, 0, 0, 0);
+  ssize_t ret = _ulmeros_syscall2(SYS_OPEN, (size_t)name, flags);
   SYSCALL_RETURN_ERRNO(ret);
 }
 
 int getpid()
 {
-  return __syscall(SYS_GETPID, 0, 0, 0, 0, 0, 0);
+  return _ulmeros_syscall0(SYS_GETPID);
 }
 
 int fork()
 {
-  ssize_t ret = __syscall(SYS_FORK, 0, 0, 0, 0, 0, 0);
+  ssize_t ret = _ulmeros_syscall0(SYS_FORK);
   SYSCALL_RETURN_ERRNO(ret);
 }
 
 int execve(char *name, char **argv, char **envp)
 {
-  ssize_t ret = __syscall(SYS_EXECVE, (sreg_t)name, (sreg_t)argv, (sreg_t)envp, 0, 0, 0);
+  ssize_t ret = _ulmeros_syscall3(SYS_EXECVE, (size_t)name, (size_t)argv, (size_t)envp);
   SYSCALL_RETURN_ERRNO(ret);
 }
 
@@ -109,19 +89,19 @@ int kill(int pid, int sig)
 
 int link(char *oldpath, char *newpath)
 {
-  ssize_t ret = __syscall(SYS_LINK, (sreg_t)oldpath, (sreg_t)newpath, 0, 0, 0, 0);
+  ssize_t ret = _ulmeros_syscall2(SYS_LINK, (size_t)oldpath, (size_t)newpath);
   SYSCALL_RETURN_ERRNO(ret);
 }
 
 int lseek(int file, int ptr, int dir)
 {
-  ssize_t ret = __syscall(SYS_LSEEK, file, (sreg_t)ptr, dir, 0, 0, 0);
+  ssize_t ret = _ulmeros_syscall3(SYS_LSEEK, file, (size_t)ptr, dir);
   SYSCALL_RETURN_ERRNO(ret);
 }
 
 caddr_t sbrk(int incr)
 {
-  return (caddr_t)__syscall(SYS_SBRK, incr, 0, 0, 0, 0, 0);
+  return (caddr_t)_ulmeros_syscall1(SYS_SBRK, incr);
 }
 
 int stat(const char *file, struct stat *st)
